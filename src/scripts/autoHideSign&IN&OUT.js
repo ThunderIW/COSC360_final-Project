365b112885ee97849aa857bf47ce455cac521335
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-03-23 14:44:34 -0700

    finsihed milestone 2

--- src/scripts/autoHideSign&IN&OUT.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const message = document.querySelector('.signup-success-message');
-    if (message) {
-        setTimeout(() => {
-            message.style.display = 'none';
-        }, 5000);
-    }
-});
